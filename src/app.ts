2d4fcfe6bb061fa8c39fa73587fbe898454e33e8
PRABIR07 <uic.19mca1007@gmail.com>
2024-01-27 19:20:39 +0530

    Add http-errors package

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,31 @@
 
-import express from 'express';
+import express, { NextFunction,Request, Response } from 'express';
+import logger from './config/logger';
+import { HttpError } from 'http-errors';
+
 
 const app = express();
+
+app.get('/', (req,res)=>{
+    res.send("Welcome to auth Service")
+})
+
+app.use((err:HttpError,req:Request,res:Response,next:NextFunction)=>{
+  
+        logger.error(err.message);
+        const statusCode = err.statusCode || 500
+        res.status(statusCode).json({
+
+            errors:[
+            {
+                type: err.name,
+                msg: err.message,
+                path: '',
+                location: ''
+            }
+            ]
+        })
+    }
+)
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
-import logger from './config/logger'
+
+
 import app from "./app";
 import { Config } from "./config";
+import logger from './config/logger';
 
 
 const startServer = ()=>{
     const PORT = Config.PORT
     try {
+        
         app.listen(PORT,()=>logger.info(`App is running on ${PORT}`))
     } catch (error) {
         console.error(error)
